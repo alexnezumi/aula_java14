8f44db23f62ebd11d4f1cafefd117fb60cc538a2
alex <alexsilva.viana2006@gmail.com>
2024-11-26 22:12:23 -0300

    atestado smt

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+ 
+package main;
+
+import Controller.Ticket_DAO;
+import View.Inicio_GUI;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+    new Inicio_GUI().setVisible(true);
+    
+    }
+    
+}
